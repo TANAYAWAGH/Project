2e4d3fe48010696a28357bdf1ebd0089a417e066
Tanaya Wagh <tanayawagh2002@gmail.com>
2024-08-09 15:15:21 +0530

    new

--- flight-booking-system/src/App.js
+++ flight-booking-system/src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import { Container } from '@mui/material';
 import Carousel from './components/Carousel';
 import ForgotPasswordForm from './components/ForgotForm';
+import AboutUsPage from './components/AboutUsPage';
 
 
 function App() {
   return (
 
-    <div>
-      <ForgotPasswordForm/>
-    </div>
+    <Container>
+     {/*  <Carousel/> */}
+     <AboutUsPage/>
+    </Container> 
+
+     
 
   );
 }
